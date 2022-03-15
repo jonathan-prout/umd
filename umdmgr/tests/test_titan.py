c9ee74bb1f0f86dfaba32222f06e1152da9a07d8
Jonathan Prout <prout@eurovision.net>
2022-03-15 17:59:55 +0100

    Added failing unit test

--- umdmgr/tests/test_titan.py
+++ umdmgr/tests/test_titan.py
@@ -26,6 +26,7 @@ class TestTitan(unittest.TestCase):
 		titan.set_get_decoder_api_channels_id_content(jdata)
 		decoder = titan.getInterface("decoder")
 		self.assertEqual(decoder.getKey("frame_rate"), 25.0)
+		self.assertEqual(decoder.getKey("aspect_ratio"), "16_9")
 		self.assertEqual(decoder.getKey("service_id"), 1)
 
 	def test_sql1_sql_generation_noerror(self):
@@ -65,6 +66,17 @@ class TestTitan(unittest.TestCase):
 		self.assertEqual(d['rolloff'], '0_20')
 		self.assertEqual(d['aspectratio'], '16_9')
 
+	def test_refresh2(self):
+		titan = ateme_titan.Titan(1, "10.88.203.21", "titan", 2)
+		fname = "get__decoder_api_channels_2_7_5.json"
+		with open(os.sep.join(["testdata", fname]), "r") as fobj:
+			jdata = json.load(fobj)
+		titan.set_get_decoder_api_channels_id_content(jdata)
+		decoder = titan.getInterface("decoder")
+		self.assertEqual(decoder.getKey("frame_rate"), 25.0)
+		self.assertEqual(decoder.getKey("aspect_ratio"), "16_9")
+		self.assertEqual(decoder.getKey("service_id"), 1)
 
+		
 if __name__ == '__main__':
 	unittest.main()
